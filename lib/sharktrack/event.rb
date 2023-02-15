fbbead430e628128ac997ff92469d9dfc41f883b
uooobarry <383435763@qq.com>
2023-02-15 16:37:16 +0800

    Add event class

--- lib/sharktrack/event.rb
+++ lib/sharktrack/event.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
+require_relative "concerns/parsable"
+
 module Sharktrack
   # Track Event
   class Event
     include Concerns::Parsable
 
-    attr_accessor :timestamp, :description
+    attr_accessor :timestamp, :description, :country, :city, :province
 
     def initialize(**params)
       parse_params(**params)
--- lib/sharktrack/response.rb
+++ lib/sharktrack/response.rb
@@ -3,12 +3,13 @@
 require "active_support/core_ext/hash/except"
 require_relative "parsers/json_parser"
 require_relative "concerns/parsable"
+require_relative "event"
 module Sharktrack
   # Common response
   class Response
     include Concerns::Parsable
 
-    attr_accessor :tracking_number, :ship_to, :estimated_delivery_date, :courier
+    attr_accessor :tracking_number, :ship_to, :estimated_delivery_date, :courier, :events
 
     def initialize(**params)
       parse_params!(**params)
