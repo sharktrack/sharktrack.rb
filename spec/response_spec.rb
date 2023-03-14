a41cd84d2bed687eac48f152052d086610446cde
uooobarry <383435763@qq.com>
2023-03-14 15:14:36 +0800

    Rename origin_body => body for better decleartion

--- spec/response_spec.rb
+++ spec/response_spec.rb
@@ -4,7 +4,7 @@ require "active_support/hash_with_indifferent_access"
 
 RSpec.describe Sharktrack::Response do
   it "can initialized and auto assign paramters to object" do
-    res = Sharktrack::Response.new(origin_body: "",
+    res = Sharktrack::Response.new(body: "",
                                    response_format: "json",
                                    courier: "UPS")
     expect(res.courier).to be("UPS")
